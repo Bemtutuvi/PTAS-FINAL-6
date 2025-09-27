5cb58a79029eb1159ca7aa11caea05d4624af82a
Juju <julia.almeida3@estudante.ifms.edu.br>
2025-09-26 21:27:15 -0400

    testes

--- auth.test.js
+++ auth.test.js
@@ -1,94 +1,74 @@
 const request = require("supertest");
 const app = require("../index");
 
-// testes de autentificação
 describe("Testes de autenticação", () => {
   let token = "";
 
-  // testar o cadastro
   it("Deve cadastrar um novo usuário com sucesso", async () => {
     const res = await request(app)
       .post("/auth/cadastro")
       .send({
-        nome: "Usuário Teste",
-        email: `teste${Date.now()}@example.com`, // evita conflito
+        nome: "Teste",
+        email: `teste${Date.now()}@example.com`, 
         password: "123456",
       });
 
-    // verificar se foi cadastrado com sucesso e enviar uma confirmação
     expect(res.statusCode).toBe(200);
     expect(res.body.erro).toBe(false);
     expect(res.body).toHaveProperty("token");
     token = res.body.token;
   });
 
-  // testar se n cadastra com email duplicado
   it("Não deve cadastrar usuário com email duplicado", async () => {
     const res = await request(app).post("/auth/cadastro").send({
-      nome: "Usuário Teste",
-      email: "duplicado@example.com",
+      nome: "Teste",
+      email: "exemplo@gmail.com",
       password: "123456",
     });
-
-    // primeiro cadastro deve ser bem sucessedido
     await request(app).post("/auth/cadastro").send({
-      nome: "Usuário Teste",
-      email: "duplicado@example.com",
+      nome: "Teste",
+      email: "exemplo@gmail.com",
       password: "123456",
     });
 
-    // segundo deve falhar
     expect(res.statusCode).toBe(400);
     expect(res.body.erro).toBe(true);
   });
 
-  // testar o login
   it("Deve logar com sucesso com usuário válido", async () => {
     const email = `login${Date.now()}@example.com`;
     await request(app).post("/auth/cadastro").send({
-      nome: "Login Teste",
+      nome: "login Teste",
       email,
       password: "123456",
     });
 
-    // tentar logar
     const res = await request(app)
       .post("/auth/login")
       .send({ email, password: "123456" });
 
-    // verificar se o login foi bem sucedido
     expect(res.statusCode).toBe(200);
     expect(res.body.erro).toBe(false);
     expect(res.body).toHaveProperty("token");
   });
-
-  // testar login com email inexistente
   it("Não deve logar com email inexistente", async () => {
     const res = await request(app).post("/auth/login").send({
-      email: "naoexiste@example.com",
+      email: "naotem@gmail.com",
       password: "123456",
     });
-
-    // verificar se o login retornando a mensagem de erro
     expect(res.statusCode).toBe(401);
     expect(res.body.erro).toBe(true);
   });
-
-  // testar login com senha incorreta
   it("Não deve logar com senha incorreta", async () => {
     const email = `senha${Date.now()}@example.com`;
     await request(app).post("/auth/cadastro").send({
-      nome: "Senha Teste",
+      nome: "Teste senha",
       email,
       password: "123456",
     });
-
-    // tentar logar com senha errada e encainhar o erro
     const res = await request(app)
       .post("/auth/login")
-      .send({ email, password: "errada" });
-
-    // verificar se o login retornando a mensagem de erro
+      .send({ email, password: "incoreta" });
     expect(res.statusCode).toBe(401);
     expect(res.body.erro).toBe(true);
   });
--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require("express");
 const app = express();
 
-
+// configurar o express para receber json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("ola mundo?");
+});
 
+// rotas de usuario
+const usuarioRoute = require("./routes/usuarioRoute");
+const UsuarioController = require("./controllers/UsuarioController");
 
-const authRoute = require("./routes/authroutes");
-const authcontroller = require("./controllers/authcontroller");
+//mudei o /usuario para /auth
+app.use("/auth", usuarioRoute);
 
-
-app.use("/auth", authRoute);
-
-
+// vereficar se vc esta logado
 app.get(
   "/areaLogada",
-  authcontroller.vereficarAutentificacao,
+  UsuarioController.vereficarAutentificacao,
   (req, res) => {
     res.json({
       msg:
@@ -27,7 +30,7 @@ app.get(
   }
 );
 
-
+// Inicia o servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Servidor rodando na porta " + PORT);
